1da2b65077ab38e9e79c4f2152e367cf3c822a63
GeoS1lva <geovanap.silva06@gmail.com>
2025-07-07 23:26:39 -0300

    front client

--- FrontEnd/js/navegação/clientes.js
+++ FrontEnd/js/navegação/clientes.js
@@ -20,3 +20,11 @@ funcionarios.addEventListener("click", () =>{
     window.location.href = "/FrontEnd/html/navegação/funcionarios.html";
 });
 
+const cadastrar = document.querySelector(".cadastrar-cliente");
+const atualizar = document.querySelector(".atualizar-cliente");
+const excluir = document.querySelector(".excluir-cliente");
+const filtrar = document.querySelector(".filtrar-cliente");
+
+cadastrar.addEventListener("click", () =>{
+    window.location.href = "/FrontEnd/html/navegação/opc-clientes/cadastrar-cliente.html"
+})
